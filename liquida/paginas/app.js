7c5e1a1939b67179d1311750ed05394c7fa5bb14
Luis-Surec <118926534+Luis-Surec@users.noreply.github.com>
2024-02-03 20:25:57 -0600

    Add files via upload
    carga de archivos

---
+++ liquida/paginas/app.js
@@ -0,0 +1,39 @@
+
+//----------------------para option  table_data  bg - warning------------------
+
+fetch('listar.php')
+.then(res=>res.json())
+.then(data=>{
+    let str = '';
+    data.map(item => {
+        str += `<option>${item.codUs} ${item.nombreUs}</option>`;
+    });
+
+    document.getElementById("table_data").innerHTML = str;
+    console.log(data);
+});
+
+
+
+
+
+//------------------------------- para  tabla_data1 color bg - info--------------------
+fetch('listar.php')
+.then(res=>res.json())
+.then(data=>{
+    let str ='';
+    data.map(item =>{
+        str += `
+        
+
+<tr>
+<td>${item.codUs}  </td>
+<td>${item.nombreUs}</td>
+<td>${item.correoUs}  </td>
+</tr>
+
+        `
+    });
+document.getElementById('table_data1').innerHTML = str;
+console.log(data);
+});

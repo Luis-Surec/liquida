2e9732277d0a24ba54a12753856d87cf8da5821b
Luis-Surec <luissurec74@gmail.com>
2024-02-15 14:04:11 -0600

    cambia  funciones

--- script/encriptador.js
+++ script/encriptador.js
@@ -1,50 +1,42 @@
-
+/*
 let  recibeDatos=document.getElementById("encriptarTexto").value;
 let resultado = sumar();
-
-function sumar(){
-    let valor1=50;
-    let valor2=2;
-    let resultado= valor1+valor2;
-return resultado;
-}
-
-
-function resta(){
-
-let resultadoSuma = sumar();
-let restante = resultadoSuma-10;
-console.log(restante+"  "+"desde funcion  resta");
-let enviaDato=document.getElementById("recibeTexto").innerHTML=  resultado + recibeDatos;
-
-}
-
-
-
-/*
-
-function sumar(){
-    let valor1=50;
-    let valor2=2;
-    let resultado= valor1+valor2;
-return resultado;
-}
-
-
-function resta(){
-
-let resultadoSuma = sumar();
-let restante = resultadoSuma-10;
-console.log(restante+"  "+"desde funcion  resta");
-let enviaDato=document.getElementById("recibeTexto").innerHTML=  recibeDatos;
-}
 */
+function sumar(){
+    let valor1=50;
+    let valor2=2;
+    let resultado= valor1+valor2;
+return resultado;
+}
+
+
+function resta(){
+let resultadoSuma = sumar();
+let restante = resultadoSuma-10;
+let  recibeDatos=document.getElementById("encriptarTexto").value;
+console.log(recibeDatos.replace(/[a,e,i,o,u]/gi, 'ai' ,'enter','iam'));
+let enviaDato=document.getElementById("recibeTexto").innerHTML="valor de:   "+restante +"mensaje:"+" "+recibeDatos;
+console.log(enviaDato);
+return restante;
+}
 
 
 
+//-----------funciona
 /*
-function procesarTexto(){
+function sumar(){
+    let valor1=50;
+    let valor2=2;
+    let resultado= valor1+valor2;
+return resultado;
+}
 
-    recibeDatos=document.getElementById("recibeTexto").innerHTML,procesarTexto;
-    console.log(procesarTexto);
+
+function resta(){
+let resultadoSuma = sumar();
+let restante = resultadoSuma-10;
+let  recibeDatos=document.getElementById("encriptarTexto").value;
+let enviaDato=document.getElementById("recibeTexto").innerHTML="valor de:   "+restante +"mensaje:"+" "+recibeDatos;
+console.log(enviaDato);
+return restante;
 }*/

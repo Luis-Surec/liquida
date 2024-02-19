ebe0c763e2116cdfb2e27a7750f57af5ff9d8f9a
Luis-Surec <luissurec74@gmail.com>
2024-02-19 14:03:42 -0600

    subiendo cambio en funciones ecnripta

--- script/encriptador.js
+++ script/encriptador.js
@@ -1,42 +1,17 @@
-/*
+function encripta(){
 let  recibeDatos=document.getElementById("encriptarTexto").value;
-let resultado = sumar();
-*/
-function sumar(){
-    let valor1=50;
-    let valor2=2;
-    let resultado= valor1+valor2;
-return resultado;
-}
-
-
-function resta(){
-let resultadoSuma = sumar();
-let restante = resultadoSuma-10;
-let  recibeDatos=document.getElementById("encriptarTexto").value;
-console.log(recibeDatos.replace(/[a,e,i,o,u]/gi, 'ai' ,'enter','iam'));
-let enviaDato=document.getElementById("recibeTexto").innerHTML="valor de:   "+restante +"mensaje:"+" "+recibeDatos;
+let textoCifrado =recibeDatos
+.replace(/a/gi, 'ia')
+.replace(/e/gi, 'enter')
+.replace(/i/gi, 'ooooo')
+.replace(/o/gi, 'ober')
+.replace(/u/gi, 'ufat');
+if( document.getElementById("textoCifrado").value.length !=0 ){
+document.getElementById("recibeDatos").value =textoCifrado;
+let enviaDato=document.getElementById("recibeTexto").innerHTML=textoCifrado;
 console.log(enviaDato);
-return restante;
+return enviaDato
 }
 
-
-
-//-----------funciona
-/*
-function sumar(){
-    let valor1=50;
-    let valor2=2;
-    let resultado= valor1+valor2;
-return resultado;
 }
 
-
-function resta(){
-let resultadoSuma = sumar();
-let restante = resultadoSuma-10;
-let  recibeDatos=document.getElementById("encriptarTexto").value;
-let enviaDato=document.getElementById("recibeTexto").innerHTML="valor de:   "+restante +"mensaje:"+" "+recibeDatos;
-console.log(enviaDato);
-return restante;
-}*/

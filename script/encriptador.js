2b92a5ab8abd08e2ddfae0d87a483fe71715763b
Luis Surec <luissurec74@gmail.com>
2024-02-19 22:12:08 -0600

    subiendo a la nube

--- script/encriptador.js
+++ script/encriptador.js
@@ -1,45 +1,67 @@
+function EnviarText(){
+
+  let recibeT= document.getElementById("textoRec").value;
+  let recibeProc = recibeT;
+  var constantes = {
+  e:"enter",
+  i:"imes",
+  a:"ai",
+  o:"ober",
+  u:"ufat",
+  A:"AI"
+
+};
+
+if(recibeT = recibeT.replace(/e|i|a|o|u/gi, function(matched){
+  return constantes[matched];
+  
+ })){
 
 
+  let enviaDato = document.getElementById("textoEnc").innerHTML=recibeT;
+  console.log( recibeT);
+ } else{
+  let enviaDato = document.getElementById("textoEnc").innerHTML="no  hay mensaje";
+console.log("no  hay mensaje");
+ };
+}
+
+
+
+
+
+
+
+
+//-----https://app.aluracursos.com/forum/topico-encriptador-de-texto-213839
+//https://www.google.com/search?q=encripatdor+conjavascritp&oq=encripatdor+conjavascritp&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIJCAEQIRgKGKABMgkIAhAhGAoYoAHSAQkxMzk3NGowajeoAgCwAgA&sourceid=chrome&ie=UTF-8#fpstate=ive&vld=cid:4eaf685d,vid:QI4Jaz3gLm4,st:0
 //------------------recibiendo texto de enviandoTexto de textArea de html--------------------------- 
+/*
+function EnviarText(){
 
-function botonEnviarTexto(){
-   let  recibeTexto = document.getElementById("encriptar").value;
-    var constantes = {
-       e:"enter",
-       i:"imes",
-       a:"ai",
-       o:"ober",
-       u:"ufat"
-    };
-    recibeTexto = recibeTexto.replace(/e|i|a|o|u/g, function(matched){
-      return constantes[matched];
-    });
-    console.log(recibeTexto);
- return(recibeTexto);
-/* si esta aca en la funcion si se puede ver, pero afuera no funciona
-    function asignarTexto(elemento,texto){
-        let elementoHTML = document.querySelector(elemento);
-        elementoHTML.innerHTML=texto;
-    }
-    
-    asignarTexto("textarea",recibeTexto);
-    asignarTexto("p",recibeTexto);
-    */
- 
+  let recibeT= document.getElementById("textoRec").value;
+  let recibeProc = recibeT;
+  var constantes = {
+  e:"enter",
+  i:"imes",
+  a:"ai",
+  o:"ober",
+  u:"ufat",
+  A:"AI"
+
+};
+
+if(recibeT = recibeT.replace(/e|i|a|o|u/gi, function(matched){
+  return constantes[matched];
+  
+ })){
+
+
+  let enviaDato = document.getElementById("textoEnc").innerHTML=recibeT;
+  console.log( recibeT);
+ } else{
+  let enviaDato = document.getElementById("textoEnc").innerHTML="no  hay mensaje";
+console.log("no  hay mensaje");
+ };
 }
-let recibe = recibeTexto(constantes);
-console.log(recibe);
-
-let recibe1 = recibeTexto;
-console.log(recibe1);
-
-
-
-function asignarTexto(elemento,texto){
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML=texto;
-}
-
-asignarTexto("textarea",recibeTexto);
-asignarTexto("p",recibeTexto);
-
+*/
